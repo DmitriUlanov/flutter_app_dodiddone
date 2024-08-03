497377aa5e8c75e316997a3a6488081ee6e9d60f
DmitriiUlanov <da.ulanov@yandex.ru>
2024-08-03 17:34:24 +0300

    login page

---
+++ lib/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DoDidDoneTheme {
+  static ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFF004D40), // Основной цвет
+      brightness: Brightness.light,
+      primary: const Color(0xFF004D40), // Основной цвет
+      onPrimary: Colors.white, // Текст на основном цвете
+      secondary: const Color(0xFFfff8e1), // Вторичный цвет
+      onSecondary: const Color(0xFF004D40), // Текст на вторичном цвете
+      error: Colors.red, // Цвет ошибки
+      onError: Colors.white, // Текст на цвете ошибки
+      background: const Color(0xFFfff8e1), // Цвет фона
+      onBackground: const Color(0xFF004D40), // Текст на фоне
+      surface: const Color(0xFFfff8e1), // Цвет поверхности
+      onSurface: const Color(0xFF004D40), // Текст на поверхности
+    ),
+    useMaterial3: true,
+    // Добавляем стиль для кнопок
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        textStyle: const TextStyle(color: Colors.white), // Белый текст
+      ),
+    ),
+  );
+}
